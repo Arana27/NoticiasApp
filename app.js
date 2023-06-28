22d9f99e51a940e68cc625da25e146973ffe3181
Arana27 <elvin.arana27@gmail.com>
2023-06-28 00:58:03 -0600

    importacion

---
+++ app.js
@@ -0,0 +1,7 @@
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+import NewList from './src/NewList';
+import NewsDetails from './src/NewsDetail';
+
+

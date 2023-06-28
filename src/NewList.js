9f51d696935aafdeed8f8ab0673b5c043ebcf9bd
Arana27 <elvin.arana27@gmail.com>
2023-06-28 00:40:24 -0600

    estilos

--- src/NewList.js
+++ src/NewList.js
@@ -40,3 +40,20 @@ const NewList = ({navigation}) => {
       </View>
     );
   };
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      padding: 16,
+    },
+    newsItem: {
+      marginBottom: 16,
+    },
+    title: {
+      fontSize: 18,
+      fontWeight: 'bold',
+    },
+    description: {
+      fontSize: 14,
+    },
+  });
